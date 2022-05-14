750c41e1af278b12cd139e866d992f1c47437a0a
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2022-05-14 23:28:37 +0200

    More main

--- src/main.cpp
+++ src/main.cpp
@@ -69,10 +69,96 @@ void finalize() {
    Distributed::finalize();
 }
 
+#define RETURN(x)   \
+   {                \
+      bool ret = x; \
+      finalize();   \
+      return ret;   \
+   }
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+EMSCRIPTEN_KEEPALIVE void processCommand(const char *str){
+    const std::string command(str);
+    UCI::processCommand(command);
+}
+#ifdef __cplusplus
+}
+#endif
+#endif
+
 void setup() {
-  // put your setup code here, to run once:
+   init(0, nullptr);
+
+#ifndef __EMSCRIPTEN__
+
+#ifdef WITH_TEST_SUITE
+   if (argc > 1 && test(argv[1])) RETURN(EXIT_SUCCESS)
+#endif // WITH_TEST_SUITE
+
+#ifdef WITH_EVAL_TUNING
+   if (argc > 1 && std::string(argv[1]) == "-tuning") {
+      evaluationTuning(argv[2]);
+      RETURN(EXIT_SUCCESS)
+   }
+#endif // WITH_EVAL_TUNING
+
+#ifdef WITH_PGN_PARSER
+   if (argc > 1 && std::string(argv[1]) == "-pgn") { RETURN(PGNParse(argv[2])) }
+#endif // WITH_PGN_PARSER
+
+#ifdef WITH_DATA2BIN
+   if (argc > 1 && std::string(argv[1]) == "-plain2bin") { RETURN(convert_plain_to_bin({argv[2]}, std::string(argv[2]) + ".bin", 1, 300)) }
+   if (argc > 1 && std::string(argv[1]) == "-pgn2bin")   { RETURN(convert_bin_from_pgn_extract({argv[2]}, std::string(argv[2]) + ".bin", true, false)) }
+   if (argc > 1 && std::string(argv[1]) == "-bin2plain") { RETURN(convert_bin_to_plain({argv[2]}, std::string(argv[2]) + ".plain")) }
+   if (argc > 1 && std::string(argv[1]) == "-rescore")   { RETURN(rescore({argv[2]}, std::string(argv[2]) + ".rescored")) }
+#endif // WITH_DATA2BIN
+
+#ifdef DEBUG_TOOL
+   std::string firstOption;
+   // if (argc < 2) {
+   //    Logging::LogIt(Logging::logInfo) << "Hint: You can use -xboard command line option to enter xboard mode";
+   //    firstOption = "-uci"; // default is uci
+   // }
+   // else
+   //    firstOption = argv[1];
+   // int ret = cliManagement(firstOption, argc, argv);
+   STOP_AND_SUM_TIMER(Total)
+#ifdef WITH_TIMER
+   Timers::Display();
+#endif // WITH_TIMER
+   finalize();
+   // return ret;
+#else // DEBUG_TOOL
+   // only init TimeMan if needed ...
+   TimeMan::init();
+
+#ifdef WITH_UCI // UCI is prefered option
+   UCI::init();
+   UCI::uci();
+#else // WITH_UCI
+#ifdef WITH_XBOARD
+   XBoard::init();
+   XBoard::xboard();
+#endif // WITH_XBOARD
+
+#endif // WITH_UCI
+
+   STOP_AND_SUM_TIMER(Total)
+
+#ifdef WITH_TIMER
+   Timers::Display();
+#endif // WITH_TIMER
+
+   RETURN(EXIT_SUCCESS)
+#endif // DEBUG_TOOL
+
+#endif // __EMSCRIPTEN__
+   // return 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
